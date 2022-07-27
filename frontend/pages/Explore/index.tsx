24746c7df9b03d23885c746bf0909f70b020f30f
punnkam <punn.kamolyabutr@gmail.com>
2022-07-26 21:33:17 -0700

    fix errors and manual merge new gallery

--- frontend/pages/Explore/index.tsx
+++ frontend/pages/Explore/index.tsx
@@ -1,11 +1,10 @@
-// http://localhost:3000/Explore
-// https://github.com/tailwindlabs/tailwindcss-aspect-ratio
-
 import React from 'react';
-import Modal from '../../components/CustomModal';
+// import Modal from '../../components/CustomModal';
+import { Box, SimpleGrid, Flex, Heading } from '@chakra-ui/react';
+import { GalleryCard } from '../../components/GalleryCard';
 import Layout from '../../components/Layout';
 
-const products = [
+export const products = [
   {
     id: 1,
     cardInfo: {
@@ -13,8 +12,12 @@ const products = [
       href: '#',
       imageSrc: 'fakenft.png',
       imageAlt: 'Fake NFT boi',
-      price: '$69',
-      color: 'Black',
+      price: 0.2,
+      creator: 'Verci',
+      recur: 'Yr',
+      share: 1,
+      sold: 10,
+      supply: 500,
     },
     modalInfo: {
       Title: 'Verci DAO',
@@ -23,43 +26,43 @@ const products = [
       Details: {
         first: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
         second: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
         third: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
       },
       Perks: {
         first: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
         second: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
         third: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
       },
       History: {
         first: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
         second: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
         third: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
       },
     },
@@ -67,12 +70,16 @@ const products = [
   {
     id: 2,
     cardInfo: {
-      name: 'Racing DAO',
+      name: 'Killa Killa',
       href: '#',
       imageSrc: 'racer.png',
       imageAlt: 'Fake NFT boi',
-      price: '$500',
-      color: 'Blue',
+      price: 0.2,
+      creator: 'Racing DAO',
+      recur: 'Yr',
+      share: 2,
+      sold: 10,
+      supply: 500,
     },
     modalInfo: {
       Title: 'Racing DAO',
@@ -129,8 +136,12 @@ const products = [
       href: '#',
       imageSrc: 'gorls.png',
       imageAlt: 'Fake NFT boi',
-      price: '$1,000',
-      color: 'Blue',
+      price: 0.2,
+      creator: 'Gorls',
+      recur: 'Yr',
+      share: 2,
+      sold: 10,
+      supply: 500,
     },
     modalInfo: {
       Title: 'Gorls DAO',
@@ -139,43 +150,43 @@ const products = [
       Details: {
         first: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
         second: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
         third: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
       },
       Perks: {
         first: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
         second: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
         third: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
       },
       History: {
         first: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
         second: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
         third: {
           text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod dui sed dui fermentum, a vestibulum leo volutpat.',
-          imageSrc: 'time.png',
+          imageSrc: 'racerdao/time.png',
         },
       },
     },
@@ -183,80 +194,31 @@ const products = [
   // More Subs...
 ];
 
-export default function Explore() {
-  const [showModal, setShowModal] = React.useState(false);
-  const [modalInfo, setModalInfo] = React.useState({});
-  const [cardInfo, setCardInfo] = React.useState({});
+export const Explore = () => (
+  <Layout>
+    <Flex direction='column'>
+      <Flex ml={200}>
+        <Heading>Explore Subscriptions</Heading>
+      </Flex>
+      <Box
+        // contains the grid of cards
+        maxW='5xl'
+        ml={200}
+        mt={105}
+        // px={{ base: '4', md: '8', lg: '12' }}
+        // py={{ base: '6', md: '8', lg: '12' }}
+      >
+        <SimpleGrid
+          columns={{ base: 1, sm: 2, md: 2, lg: 3 }}
+          gap={{ base: '8', lg: '12' }}
+        >
+          {products.map((product) => (
+            <GalleryCard key={product.id} product={product} />
+          ))}
+        </SimpleGrid>
+      </Box>
+    </Flex>
+  </Layout>
+);
 
-  const toggleModal = () => {
-    setShowModal(!showModal);
-  };
-  return (
-    <Layout>
-      <div className='flex flex-col flex-1 md:pl-20'>
-        <div>
-          <div className='max-w-2xl px-4 py-16 mx-auto sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
-            <h2 className='text-2xl text-gray-900 font-chakra'>
-              Explore Subscriptions
-            </h2>
-
-            <div className='grid grid-cols-1 mt-6 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
-              {products.map((product) => {
-                const { name, href, imageSrc, imageAlt, price, color } =
-                  product.cardInfo;
-                return (
-                  <button
-                    key={product.id}
-                    type='button'
-                    onClick={() => {
-                      setCardInfo(product.cardInfo);
-                      setModalInfo(product.modalInfo);
-                      setShowModal(true);
-                    }}
-                  >
-                    <div key={product.id} className='relative group'>
-                      <div className='w-full overflow-hidden bg-gray-200 rounded-md min-h-80 aspect-w-1 aspect-h-1 group-hover:opacity-75 lg:h-80 lg:aspect-none '>
-                        <img
-                          src={imageSrc}
-                          alt={imageAlt}
-                          className='object-cover object-center w-full h-full lg:w-full lg:h-full '
-                        />
-                      </div>
-                      <div className='flex justify-between mt-4'>
-                        <div>
-                          <h3 className='text-sm text-gray-700'>
-                            <a href={href}>
-                              <span
-                                aria-hidden='true'
-                                className='absolute inset-0 font-chakra'
-                              />
-                              {name}
-                            </a>
-                          </h3>
-                          <p className='mt-1 text-sm text-gray-500 font-chakra'>
-                            {color}
-                          </p>
-                        </div>
-                        <p className='text-sm font-medium text-gray-900 font-chakra'>
-                          {price}
-                        </p>
-                      </div>
-                    </div>
-                  </button>
-                );
-              })}
-            </div>
-            {showModal ? (
-              <Modal
-                modalInfo={modalInfo}
-                cardInfo={cardInfo}
-                toggleModal={toggleModal}
-                isOpen={showModal}
-              />
-            ) : null}
-          </div>
-        </div>
-      </div>
-    </Layout>
-  );
-}
+export default Explore;
